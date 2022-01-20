37b5c9866db97d5452e62025fc223754fe41ab15
orsoxii <orsooo@mailforkids.net>
2022-01-20 17:52:21 +0100

    2 files modified, 1 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# js-yypg3w
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-yypg3w)
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
-
+import "./index.html"
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const app = document.getElementById("app")
+function execute(){
+  return "<p>hello</p>"
+}
+app.innerHTML = execute()
